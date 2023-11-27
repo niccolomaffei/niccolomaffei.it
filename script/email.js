0e3d745fd577b2b45d16430a09708f8d5b878fd5
[niccolomaffei] <niccomaffei@gmail.com>
2023-11-27 14:15:36 +0100

    -

--- script/email.js
+++ script/email.js
@@ -1,15 +1,36 @@
-/*
+// var params = {
+//   name : document.getElementById("name"),
+//   email : document.getElementById("email").value,
+//   subject : document.getElementById("subject").value,
+//   message : document.getElementById("message").value,
+// };
 
-function sendMail(){
-  let parameters = {
-    name : document.getElementById("name").value,
-    email : document.getElementById("email").value,
-    subject : document.getElementById("subject").value,
-    message : document.getElementById("message").value
-  }
+// const serviceID = "service_a8d1bxp";
+// const templateID = "template_ce99ea5";
 
-  emailjs.send("service_a8d1bxp", "template_ce99ea5", parameters).then(alert("Email inviata con successo!"))
-}
+// emailjs.send(serviceID, templateID, params)
+//   .then(function(response) {
+//      console.log('SUCCESS!', response.status, response.text);
+//   }, function(error) {
+//      console.log('FAILED...', error);
+//   });
 
-*/
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
+    const serviceID = "service_a8d1bxp";
+    const templateID = "template_ce99ea5";
+
+    emailjs.sendForm(serviceID, templateID, this).then(
+      (response) => {
+        console.log("SUCCESS!", response.status, response.text);
+        alert("SUCCESS!");
+      },
+      (error) => {
+        console.log("FAILED...", error);
+        alert("FAILED...", error);
+      }
+    );
+  });

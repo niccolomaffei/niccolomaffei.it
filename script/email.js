d4fb59454e74225004881e5d0a50a831603d6aa4
[niccolomaffei] <niccomaffei@gmail.com>
2023-11-27 14:38:36 +0100

    -

--- script/email.js
+++ script/email.js
@@ -15,22 +15,60 @@
 //      console.log('FAILED...', error);
 //   });
 
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+// document
+//   .getElementById("contact-form")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault();
 
-    const serviceID = "service_a8d1bxp";
-    const templateID = "template_ce99ea5";
+//     const serviceID = "service_a8d1bxp";
+//     const templateID = "template_ce99ea5";
 
-    emailjs.sendForm(serviceID, templateID, this).then(
-      (response) => {
-        console.log("SUCCESS!", response.status, response.text);
-        alert("SUCCESS!");
-      },
-      (error) => {
-        console.log("FAILED...", error);
-        alert("FAILED...", error);
-      }
-    );
-  });
+//     emailjs.sendForm(serviceID, templateID, this).then(
+//       (response) => {
+//         console.log("SUCCESS!", response.status, response.text);
+//         alert("SUCCESS!");
+//       },
+//       (error) => {
+//         console.log("FAILED...", error);
+//         alert("FAILED...", error);
+//       }
+//     );
+//   });
+
+//Getting the name and email from the DOM
+let name = document.getElementById('name').value;
+let email = document.getElementById('email').value;
+let subject = document.getElementById('subject').value;
+let message = document.getElementById('message').value;
+
+//Getting the button from the DOM
+let submitButton = document.getElementById('button') 
+
+const serviceID = "service_a8d1bxp";
+const templateID = "template_ce99ea5";
+
+//Add event listener on click to the button - notice i added the event as argument to the function
+submitButton.addEventListener('click', function(event){
+
+    //prevent the reload of the page. here i prevent the event.
+    event.preventDefault()
+
+    //Sending the email with the name and email
+    emailjs.send(serviceID, templateID, {
+        "name": name,
+        "email": email,
+        "subject": subject,
+        "message": message
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                
+            },
+            function (error) {
+                console.log("FAILED", error);
+                
+            }
+
+        );
+})
